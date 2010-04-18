3861d1450b1a12b4b50d8e0b2b0beec7a0023d71
Guillermo Álvarez <guillermo@cientifico.net>
2010-04-18 23:09:49 +0200

    Respond to xml

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -1,7 +1,10 @@
 class DeputiesController < ApplicationController
   def index
-    
     @deputies = Deputy.by_initiatives
+    respond_to do |wants|
+      wants.html
+      wants.xml { render :xml => @deputies.to_xml}
+    end
   end
 
 end
